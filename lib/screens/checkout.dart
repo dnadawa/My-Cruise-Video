e30f21c76c19794e0c57df73ce49ec49d6f67aa4
Sanjuwa <Sanjulahasaranga0@gmail.com>
2020-06-21 00:39:42 +0530

    Checkout Updated

--- lib/screens/checkout.dart
+++ lib/screens/checkout.dart
@@ -8,17 +8,12 @@ import 'package:flutter_credit_card/credit_card_form.dart';
 import 'package:flutter_credit_card/credit_card_model.dart';
 import 'package:flutter_credit_card/credit_card_widget.dart';
 import 'package:mycruisevideo/widgets/button.dart';
+import 'package:mycruisevideo/widgets/custom-text.dart';
 import 'package:mycruisevideo/widgets/toast.dart';
 
 import 'package:stripe_payment/stripe_payment.dart';
 
 class Checkout extends StatefulWidget {
-  final String orderID;
-  final int price;
-  final String bakeryName;
-  final String bakeryEmail;
-  final String userEmail;
-  const Checkout({Key key, this.orderID, this.price, this.bakeryName, this.bakeryEmail, this.userEmail}) : super(key: key);
   @override
   _CheckoutState createState() => _CheckoutState();
 }
@@ -29,6 +24,7 @@ class _CheckoutState extends State<Checkout> {
   String cardHolderName='';
   String cvvCode = '';
   bool isCvvFocused = false;
+  bool isSelected = false;
 
   void onCreditCardModelChange(CreditCardModel creditCardModel) {
     setState(() {
@@ -56,6 +52,7 @@ class _CheckoutState extends State<Checkout> {
   Widget build(BuildContext context) {
     ScreenUtil.init(width: 720, height: 1520,allowFontScaling: false);
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         centerTitle: true,
         elevation: 0,
@@ -75,89 +72,110 @@ class _CheckoutState extends State<Checkout> {
             image: DecorationImage(image: AssetImage('images/back.png'),fit: BoxFit.fill)
         ),
 
-        child: Column(
-          children: <Widget>[
-            CreditCardWidget(
-              cardNumber: cardNumber,
-              expiryDate: expiryDate,
-              cardHolderName: cardHolderName,
-              cvvCode: cvvCode,
-              showBackView: isCvvFocused,
-              cardBgColor: Theme.of(context).primaryColor,
-            ),
-            Expanded(
-              child: SingleChildScrollView(
-                child: CreditCardForm(
-                  textColor: Colors.white,
-                  themeColor: Colors.white,
-                  cardHolderName: cardHolderName,
-                  cardNumber: cardNumber,
-                  expiryDate: expiryDate,
-                  onCreditCardModelChange: onCreditCardModelChange,
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            children: <Widget>[
+              CreditCardWidget(
+                cardNumber: cardNumber,
+                expiryDate: expiryDate,
+                cardHolderName: cardHolderName,
+                cvvCode: cvvCode,
+                showBackView: isCvvFocused,
+                cardBgColor: Theme.of(context).primaryColor,
+              ),
+              CreditCardForm(
+                textColor: Colors.black,
+                themeColor: Colors.black,
+                cardHolderName: cardHolderName,
+                cardNumber: cardNumber,
+                expiryDate: expiryDate,
+                onCreditCardModelChange: onCreditCardModelChange,
+              ),
+              Padding(
+                padding:  EdgeInsets.all(ScreenUtil().setHeight(40)),
+                child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(image: AssetImage('images/gradient.jpg'))
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        width: double.infinity,
+                        height: ScreenUtil().setHeight(100),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.white,
+                          border: Border.all(color: Theme.of(context).primaryColor,width: 4)
+                        ),
+                        child: Center(child: CustomText(text: 'AGREEMENT',color: Colors.black,size: ScreenUtil().setSp(45),)),
+                      ),
+                      Padding(
+                        padding:  EdgeInsets.all(ScreenUtil().setHeight(20)),
+                        child: CustomText(
+                          text: 'I, hereby grant permission to My Cruise Video to use or reproduce any portion of the photo images provided through the current app that have been taken by myself or someone I designated '
+                              'for the purpose of a personalized video and or publications which can include but is not limited to, books, cards, calendars, invitations and websites without any more compensation or '
+                              'recognition given to me. Furthermore, I grant creative permission to alter the photograph(s) or video’. I do not grant permission to resale or use the photographs in a manner that would '
+                              'exploit or cause malicious representation toward me or my company and associates.',
+                          align: TextAlign.left,
+                          size: ScreenUtil().setSp(32),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-//            Expanded(child: Container()),
-            Align(
-                alignment: Alignment.bottomCenter,
-                child: Button(text: 'Pay',onclick: (){
-                  ToastBar(text: 'Please wait',color: Colors.orange).show();
-                  print(widget.price);
-                  try{
-                    final CreditCard testCard = CreditCard(
-                      number: cardNumber,
-                      expMonth: int.parse(expiryDate[0]+expiryDate[1]),
-                      expYear: int.parse(expiryDate[3]+expiryDate[4]),
-                      cvc: cvvCode,
-                    );
 
-                    //print(int.parse(expiryDate[3]+expiryDate[4]));
-                    StripePayment.createTokenWithCard(
-                      testCard,
-                    ).then((token) async {
-                      try{
-                        var response = await http.post('https://api.stripe.com/v1/charges',
-                          body: {'amount': '${(widget.price*100).toString()}','currency': 'usd',"source": token.tokenId},
-                          headers: {'Authorization': "Bearer sk_test_NxydE0BKuePH2kP6BV0MCc2200J0phDPbk"},
-                        );
-                        print('Response status: ${response.statusCode}');
-                        print('Response body: ${response.body}');
 
-                        if(response.statusCode == 200){
-                          await ToastBar(text: 'Payment Completed!',color: Colors.green).show();
-                          Firestore.instance.collection('request').document(widget.orderID).updateData({
-                            'status': 'Processing',
-                            'activeBaker': widget.bakeryEmail
-                          });
-                          Firestore.instance.collection('request').document(widget.orderID).collection('offers').document(widget.bakeryEmail).updateData({
-                            'isActive': true
-                          });
-                          Firestore.instance.collection('orders').document(widget.orderID).setData({
-                            'id': widget.orderID,
-                            'bakeryName': widget.bakeryName,
-                            'bakeryEmail': widget.bakeryEmail,
-                            'price': widget.price,
-                            'userEmail': widget.userEmail,
-                            'status': 'Processing',
-                            'withdrawn': false
-                          });
-                          ToastBar(text: 'Data Uploaded!',color: Colors.green).show();
 
-                        }
-                        else{
-                          ToastBar(text: 'Something went Wrong While Processing the Payment',color: Colors.red).show();
-                        }
-                      }
-                      catch(e){
-                        ToastBar(text: 'Something went Wrong While Processing the Payment',color: Colors.red).show();
-                      }
-                    });
-                  }
-                  catch(e){
-                    ToastBar(text: 'Something went Wrong While Processing the Payment',color: Colors.red).show();
-                  }
-                },)),
-          ],
+//            Align(
+//                alignment: Alignment.bottomCenter,
+//                child: Button(text: 'Pay',onclick: (){
+//                  ToastBar(text: 'Please wait',color: Colors.orange).show();
+//
+//                  try{
+//                    final CreditCard testCard = CreditCard(
+//                      number: cardNumber,
+//                      expMonth: int.parse(expiryDate[0]+expiryDate[1]),
+//                      expYear: int.parse(expiryDate[3]+expiryDate[4]),
+//                      cvc: cvvCode,
+//                    );
+//
+//                    //print(int.parse(expiryDate[3]+expiryDate[4]));
+//                    StripePayment.createTokenWithCard(
+//                      testCard,
+//                    ).then((token) async {
+//                      try{
+//                        var response = await http.post('https://api.stripe.com/v1/charges',
+//                          body: {'amount': '7900','currency': 'usd',"source": token.tokenId},
+//                          headers: {'Authorization': "Bearer sk_test_NxydE0BKuePH2kP6BV0MCc2200J0phDPbk"},
+//                        );
+//                        print('Response status: ${response.statusCode}');
+//                        print('Response body: ${response.body}');
+//
+//                        if(response.statusCode == 200){
+//                          await ToastBar(text: 'Payment Completed!',color: Colors.green).show();
+//
+//                          ToastBar(text: 'Data Uploaded!',color: Colors.green).show();
+//
+//                        }
+//                        else{
+//                          ToastBar(text: 'Something went Wrong While Processing the Payment',color: Colors.red).show();
+//                        }
+//                      }
+//                      catch(e){
+//                        ToastBar(text: 'Something went Wrong While Processing the Payment',color: Colors.red).show();
+//                      }
+//                    });
+//                  }
+//                  catch(e){
+//                    ToastBar(text: 'Something went Wrong While Processing the Payment',color: Colors.red).show();
+//                  }
+//                },)),
+            ],
+          ),
         ),
 
       ),
